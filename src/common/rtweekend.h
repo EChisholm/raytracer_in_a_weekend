481033263e717be5633f8f820cf8ab18808d5e0a
Evan Chisholm <evan.chisholm@gmail.com>
2020-04-05 17:20:05 -0700

    added camera and antialiasing

--- src/common/rtweekend.h
+++ src/common/rtweekend.h
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <limits>
 #include <memory>
+#include <functional>
+#include <random>
 
 //Usings
 using std::shared_ptr;
@@ -22,6 +24,26 @@ inline double degrees_to_radians(double degrees){
 
 inline double ffmin(double a, double b){ return a <= b ? a : b; }
 inline double ffmax(double a, double b){ return a >= b ? a : b; }
+
+inline double random_double() {
+    //Returns a random real in [0,1)
+    static std::uniform_real_distribution<double> distribution(0.0,1.0);
+    static std::mt19937 generator;
+    static std::function<double()> rand_generator =
+        std::bind(distribution, generator);
+    return rand_generator();
+}
+
+inline double random_double(double min, double max) {
+    //Returns a random real in [min, max)
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max){
+    if (x < min) return min;
+    if (x > max) return max;
+    return x; 
+}
 
 //Common Headers
 #include "ray.h"
