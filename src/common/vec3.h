0197da5998efea5997ce7340c82985adc00e4130
Evan Chisholm <evan.chisholm@gmail.com>
2020-04-05 17:55:42 -0700

    added diffuse surface rendering

--- src/common/vec3.h
+++ src/common/vec3.h
@@ -44,17 +44,27 @@ class vec3{
         }
 
         void write_color(std::ostream &out, int samples_per_pixel) {
-            //Divide the color total by the number of pixels
+            //Divide the color total by the number of pixels and gamma-correct for gamma-value of 2.0
             auto scale = 1.0/samples_per_pixel;
-            auto r = scale*e[0];
-            auto g = scale*e[1];
-            auto b = scale*e[2];
+            auto r = sqrt(scale*e[0]);
+            auto g = sqrt(scale*e[1]);
+            auto b = sqrt(scale*e[2]);
 
             //Write the translated [0,255] value of each color component.
             out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
                 << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
                 << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
         }
+
+        inline static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
+
+        inline static vec3 random(double min, double max){
+            return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+        }
+
+
         public:
             double e[3];
 };
@@ -102,5 +112,28 @@ class vec3{
     
         inline vec3 unit_vector(vec3 v) {
             return v/v.length();
+        }
+
+        vec3 random_unit_vector() {
+            auto a = random_double(0, 2*pi);
+            auto z = random_double(-1,1);
+            auto r = sqrt( 1 - z*z);
+            return vec3(r*cos(a), r*sin(a), z);
+        }
+        
+        vec3 random_in_unit_sphere(){
+            while(true){
+                auto p = vec3::random(-1, 1);
+                if (p.length_squared() >= 1) continue;
+                return p;
+            }
+        }
+
+        vec3 random_in_hemisphere( const vec3& normal){
+            vec3 in_unit_sphere = random_in_unit_sphere();
+            if (dot(in_unit_sphere, normal) > 0.0) //In the same hemisphere as the normal
+                return in_unit_sphere;
+            else
+                return -in_unit_sphere;
         }
 #endif
--- src/weekend/main.cc
+++ src/weekend/main.cc
@@ -6,10 +6,19 @@
 
 #include <iostream>
 
-vec3 ray_color( const ray& r, const hittable& world){
+vec3 ray_color( const ray& r, const hittable& world, int depth){
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) {
-        return 0.5* (rec.normal + vec3(1,1,1));
+
+    //If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return vec3(0,0,0);
+
+    //Ignore hits near zero as well
+    if (world.hit(r, 0.001, infinity, rec)) {
+
+        vec3 target = rec.p + rec.normal +random_unit_vector(); // Lambertian diffuse renderer
+        //vec3 target = rec.p + random_in_hemisphere(rec.normal); uniform scatter diffuse renderer
+        return 0.5* ray_color(ray(rec.p, target-rec.p), world, depth-1);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
@@ -20,6 +29,7 @@ int main(){
     const int image_width = 200;
     const int image_height = 100;
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     //TODO: Look into stb_image.h to produce more image types
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -41,7 +51,7 @@ int main(){
                 auto u = (i + random_double()) / image_width;    
                 auto v = (j + random_double()) / image_height;
                 ray r = cam.get_ray(u,v);
-                color += ray_color(r, world);
+                color += ray_color(r, world, max_depth);
             }
             color.write_color(std::cout, samples_per_pixel);
         } 
